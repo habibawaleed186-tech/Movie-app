df6993f8841c089ed2725c1f285cde3ae623c950
Habiba <habibawaleed186@gmail.com>
2026-08-31 16:05:51 +0300

    basic_of_project

---
+++ lib/core/config/app_color.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class AppColor {
+  static const Dark = Color(0xFF121312);
+  static const primaryColor = Color(0xFF282A28);
+  static const yellow = Color(0xFFFFBB3B);
+  static const white = Color(0xFFFFFFFF);
+  static const green = Color(0xFF57AA53);
+  static const red = Color(0xFFE82626);
+
+}
---
+++ lib/modules/on_boarding_screen/on_boarding_view/on_boarding.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class OnBoarding extends StatelessWidget {
+  OnBoarding({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+    );
+  }
+
+}
