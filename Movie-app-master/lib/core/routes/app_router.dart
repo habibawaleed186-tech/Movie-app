84010a86ac9fb719137c4428f584ac1262c7febe
DINARAMADAN <dinaramadan232@gmail.com>
2026-09-01 12:53:11 +0300

    initial

---
+++ Movie-app-master/lib/core/routes/app_router.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:movie_app/modules/Auth_screen/login_view/login.dart';
+import 'package:movie_app/modules/Auth_screen/register_view/register.dart';
+import 'package:movie_app/modules/Home_screen/update_view/update.dart';
+import 'package:movie_app/modules/Splach_screen/splach.dart';
+import 'package:movie_app/modules/on_boarding_screen/on_boarding_view/on_boarding.dart';
+
+import '../../modules/Auth_screen/reset_password_view/reset_password.dart';
+import 'app_routes.dart';
+
+abstract class AppRouter {
+
+  static Route<dynamic>? onGenerateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case AppRoutes.Splach:
+        return MaterialPageRoute(builder: (context) => Splach());
+
+      case AppRoutes.onBoarding:
+        return MaterialPageRoute(builder: (context) => OnBoarding());
+
+      case AppRoutes.loginView:
+        return MaterialPageRoute(builder: (context) => Login());
+
+      case AppRoutes.registerView:
+        return MaterialPageRoute(builder: (context) => Register());
+
+      case AppRoutes.forgetPassword:
+        return MaterialPageRoute(builder: (context) => ResetPassword());
+      case AppRoutes.updateProfile:
+        return MaterialPageRoute(builder: (context) => Update());
+    }
+  }
+
+
+}
